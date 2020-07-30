6fd0d1703e45718b07dc7d18e34abfcea4856cce
Lanzhijiang <lanzhijiang@foxmail.com>
2020-07-30 13:28:27 +0800

    「添加」各请求对应函数存储

---
+++ network_communicator/handler/handle_function.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# author: Lan_zhijiang
+# description: 存储http/websocket请求的对应处理函数的类
+
+from network_communicator.connection_manager.websocket_server_manager import AlrCloudWebsocketServerManager
+
+
+class AlrCloudHttpHandleFunction():
+
+    def __init__(self):
+
+        self.httpHandleFunctionList = {
+            "AlrCloudWebsocketServer.sign_up_permission": AlrCloudWebsocketServerManager().sign_up_permission
+        }

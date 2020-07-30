e7ec11973d4398625310d89cee31ec7d95aa9167
Lanzhijiang <lanzhijiang@foxmail.com>
2020-07-30 13:49:51 +0800

    「更新」大致完成httpHandler

--- network_communicator/handler/http_handler.py
+++ network_communicator/handler/http_handler.py
@@ -4,6 +4,7 @@
 
 import json
 from main_system.log import AlrCloudLog
+from network_communicator.handler.handle_function import AlrCloudHttpHandleFunction
 
 
 class AlrCloudHttpHandler():
@@ -12,6 +13,8 @@ class AlrCloudHttpHandler():
 
         self.request_data = None
         self.api_list = json.load(open("./data_folder/file/api_list.json", "r", encoding="utf-8"))
+        self.standard_response = json.load(open("./data_folder/file/standard_response.json", "r", encoding="utf-8"))
+        self.handle_function = AlrCloudHttpHandleFunction()
         self.log = AlrCloudLog()
 
     def set_request_data(self, request_data):
@@ -29,10 +32,21 @@ class AlrCloudHttpHandler():
         开始处理请求
         :return:
         """
+        response = self.standard_response
         processed_command_count = 0
+
         for nowProcessingCommand in self.request_data["event"]["request"]:
             for waitConfirmCommand in self.api_list:
                 if waitConfirmCommand["commandName"] == nowProcessingCommand["commandName"]:
                     self.log.add_log(1, "HttpHandler: Start processing command: " + nowProcessingCommand["commandName"])
-                    processed_command_count+=1
 
+                    processed_command_count += 1
+                    response = self.handle_function.httpHandleFunctionList[nowProcessingCommand["commandName"]](nowProcessingCommand["param"], response)
+            if processed_command_count == 0:
+                self.log.add_log(3, "HttpHandler: There is no command compare with! CommandName: " + nowProcessingCommand["commandName"])
+                response["event"]["response"][nowProcessingCommand["commandName"]] = {
+                    "status": 1,
+                    "errMsg": "No compared command in the api list"
+                }
+
+        response["head"]["timeStamp"] = self.log.get_time_stamp()

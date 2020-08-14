433927131298ebd1b7424dea9cab8927b9c58297
Lanzhijiang <lanzhijiang@foxmail.com>
2020-08-14 23:38:21 +0800

    「修复」参数传递bug

--- network_communicator/connection_manager/websocket_server_manager.py
+++ network_communicator/connection_manager/websocket_server_manager.py
@@ -12,14 +12,15 @@ class AlrCloudWebsocketServerManager():
 
         self.log = AlrCloudLog()
 
-    def sign_up_permission(self, account, password):
+    def sign_up_permission(self, param):
 
         """
         注册websocket请求权限
-        :param account: 账户名称
-        :param password: 账户密码
+        :param param
         :return:
         """
+        account = param["account"]
+        password = param["password"]
         self.log.add_log(1, "WebsocketServer: Sign up permission for: " + account)
         account_password_list = json.load(open("./data_folder/file/websocket_account_password_list.json", "r", encoding="utf-8"))
         account_password_list.append({"account": account, "password": password})

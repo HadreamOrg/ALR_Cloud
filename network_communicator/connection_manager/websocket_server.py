96ab8ffac377a5d480f024f0540384205eddd130
Lanzhijiang <lanzhijiang@foxmail.com>
2020-08-15 00:44:38 +0800

    「更新」支持log类传递

--- network_communicator/connection_manager/websocket_server.py
+++ network_communicator/connection_manager/websocket_server.py
@@ -5,14 +5,17 @@
 import websockets
 import asyncio
 import json
+from network_communicator.handler.websocket_handler import AlrCloudWebsocketHandler
 
 
 class AlrCloudWebsocketServer():
 
-    def __init__(self):
+    def __init__(self, class_log):
 
         self.cloud_setting = json.load(open("./setting/cloud_setting/basic_setting.json", "r", encoding="utf-8"))
         self.websocket = websockets.serve(self.main_logical, self.cloud_setting["ip"], self.cloud_setting["websocketPort"])
+        self.acwh = AlrCloudWebsocketHandler(class_log)
+        self.log = class_log
 
     async def confirm_permission(self, websocket):
 
@@ -21,16 +24,21 @@ class AlrCloudWebsocketServer():
         :param websocket:
         :return:
         """
+        self.log.add_log(1, "WebsocketServer: Start confirming permission")
+
         is_pass = False
         account_password_list = json.load(open("./data_folder/file/websocket_account_password_list.json", "r", encoding="utf-8"))
         account = await websocket.recv()
         password = await websocket.recv()
         for waitConfirm in account_password_list:
             if waitConfirm["account"] == account and waitConfirm["password"] == password:
+                self.log.add_log(1, "WebsocketServer: Confirm permission success")
                 websocket.send("0")
                 is_pass = True
         if not is_pass:
+            self.log.add_log(2, "WebsocketServer: Confirm permission failed: AP is not paired")
             websocket.send("1")
+            websocket.close()
 
     async def main_logical(self, websocket, path):
 
@@ -39,4 +47,5 @@ class AlrCloudWebsocketServer():
         :return:
         """
         await self.confirm_permission(websocket)
+        await self.acwh.handle(websocket)
 
